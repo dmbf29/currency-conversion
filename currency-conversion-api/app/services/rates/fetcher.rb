91138ddb1deb6f89973604f5f620d969ff74962b
Douglas Berkley <douglasmberkley@gmail.com>
2025-08-09 11:48:30 +0900

    added a service object to fetch the rate from frankfurter

---
+++ currency-conversion-api/app/services/rates/fetcher.rb
@@ -0,0 +1,20 @@
+require "net/http"
+
+module Rates
+  class Fetcher
+    BASE_URL = "https://api.frankfurter.app"
+
+    # Returns { rate: BigDecimal, fetched_at: Time }
+    def self.call(from:, to:)
+      url = URI("#{BASE_URL}/latest?from=#{from}&to=#{to}")
+      res = Net::HTTP.get_response(url)
+      p data = JSON.parse(res.body)
+      rate = data.dig("rates", to)
+      date = data["date"]
+      {
+        rate: BigDecimal(rate.to_s),
+        fetched_at: Time.parse("#{date} 00:00:00")
+      }
+    end
+  end
+end
