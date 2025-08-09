f80c9009f03616b2eec6a9786368477a9fe2921d
Douglas Berkley <douglasmberkley@gmail.com>
2025-08-09 11:49:24 +0900

    catching api failures in the fetch

--- currency-conversion-api/app/services/rates/fetcher.rb
+++ currency-conversion-api/app/services/rates/fetcher.rb
@@ -8,9 +8,11 @@ module Rates
     def self.call(from:, to:)
       url = URI("#{BASE_URL}/latest?from=#{from}&to=#{to}")
       res = Net::HTTP.get_response(url)
+      raise "Frankfurter error: #{res.code}" unless res.is_a?(Net::HTTPSuccess)
       p data = JSON.parse(res.body)
       rate = data.dig("rates", to)
       date = data["date"]
+      raise "Rate missing" unless rate && date
       {
         rate: BigDecimal(rate.to_s),
         fetched_at: Time.parse("#{date} 00:00:00")
